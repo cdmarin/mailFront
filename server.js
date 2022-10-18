f7c2b7098d397f3a30c713ebec8466d1a69f8b06
Carlos <cdmarin@iesfuentesanluis.org>
2022-10-18 23:51:31 +0200

    asd

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/mailFront'));
+app.use(express.static('./dist/mail-front'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', { root: 'dist/mailFront/' }),
+    res.sendFile('index.html', { root: 'dist/mail-front/' }),
 );
 
 // Start the app by listening on the default Heroku port
