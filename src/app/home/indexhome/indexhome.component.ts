c86e9715504760d5415bdadeab25741ce97436ee
Carlos <cdmarin@iesfuentesanluis.org>
2022-10-28 19:30:43 +0200

    asd

--- src/app/home/indexhome/indexhome.component.ts
+++ src/app/home/indexhome/indexhome.component.ts
@@ -10,11 +10,13 @@ export class IndexhomeComponent implements OnInit {
   interval: any;
   hora1!: string[];
   hora2!: string[];
-  currentHora: string[] = ['18', '00', '00'];
+  currentHora: string[] = ['19', '50', '00'];
   currentAux: string[] = ['18', '30', '00'];
   user = "user1";
   horas: string[] = [];
   minSec: string[] = [];
+  tiempoFinal = false;
+  tiempoParpaedo = false;
   // 
   dragging = false;
   animating = false;
@@ -31,23 +33,18 @@ export class IndexhomeComponent implements OnInit {
     for (let i = 0; i < 60; i++) {
       this.minSec[i] = this.formatNumber(i);
     }
-    // this.mail.setData('correito@gmail.com').subscribe({
-    //   next: (data) => {
-    //     console.log(data);
-    //   }
-    // });
     this.setDate();
   }
 
   setDate() {
     clearInterval(this.interval);
-    this.hora1 = this.getCurrentDate(this.currentHora[0], this.currentHora[1], this.currentHora[2]);
-    this.hora2 = this.getCurrentDate(this.currentAux[0], this.currentAux[1], this.currentAux[2]);
+    this.hora1 = this.getCurrentDate(this.currentHora[0], this.currentHora[1], this.currentHora[2], 1);
+    this.hora2 = this.getCurrentDate(this.currentAux[0], this.currentAux[1], this.currentAux[2], 2);
 
     this.interval = setInterval(() => {
-      this.hora1 = this.getCurrentDate(this.currentHora[0], this.currentHora[1], this.currentHora[2]);
-      this.hora2 = this.getCurrentDate(this.currentAux[0], this.currentAux[1], this.currentAux[2]);
-    }, 1000)
+      this.hora1 = this.getCurrentDate(this.currentHora[0], this.currentHora[1], this.currentHora[2], 1);
+      this.hora2 = this.getCurrentDate(this.currentAux[0], this.currentAux[1], this.currentAux[2], 2);
+    }, 1000);
   }
 
   formatNumber(num: number) {
@@ -57,7 +54,7 @@ export class IndexhomeComponent implements OnInit {
     });
   }
 
-  getCurrentDate(horaS: string, minS: string, secS: string): string[] {
+  getCurrentDate(horaS: string, minS: string, secS: string, reloj: number): string[] {
     const fecha = new Date();
 
     let hora = parseInt(horaS);
@@ -70,6 +67,7 @@ export class IndexhomeComponent implements OnInit {
 
     if (secx < 0) {
       secx = 59 - (fecha.getSeconds() - sec);
+      minx--;
     }
 
     if (minx < 0) {
@@ -79,6 +77,25 @@ export class IndexhomeComponent implements OnInit {
 
     if (horax < 0) {
       horax = (23 - fecha.getHours()) + hora;
+    }
+    if ((reloj == 1 && (this.claseT1 == '' || this.claseT1 == 'mostrar')) ||
+      (reloj == 2 && (this.claseT2 == '' || this.claseT2 == 'mostrar'))) {
+
+      if (horax == 0 && minx <= 30) {
+        this.tiempoParpaedo = true
+      }
+      else {
+        this.tiempoParpaedo = false;
+      }
+
+      if (
+        horax == 0 && minx == 0 && secx == 0) {
+        // 
+        this.tiempoFinal = true;
+        setTimeout(() => {
+          this.tiempoFinal = false;
+        }, 10000);
+      }
     }
 
     return [this.formatNumber(horax), this.formatNumber(minx), this.formatNumber(secx)];
